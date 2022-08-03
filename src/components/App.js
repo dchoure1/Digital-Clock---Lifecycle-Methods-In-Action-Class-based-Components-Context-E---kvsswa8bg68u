574d2eecccc5e94722b4d7bf19873e77a5058b7c
Dnyaneshwar Choure <100285778+dchoure1@users.noreply.github.com>
2022-08-03 21:16:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const [d,setD] = useState(new Date());
+ 
+function cDate(){
+  setD(new Date);
+}
+
+const timerId = setInterval(cDate, 1000);
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{d.toLocaleString()}</div>
+      
     </div>
   )
 }
